3d6240c8672d69bfcda0b3a53fb554913c6a88ea
Gaba <158607248+Gabavereda@users.noreply.github.com>
2025-12-08 22:10:15 -0300

    fix: ensure MongoDB client connects before exporting database

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,9 +4,9 @@
 import { MongoClient } from "mongodb";
 
 const URI = process.env.MONGO_URI;
-
-
 const client = new MongoClient(URI);
+
+await client.connect();  
 
 export const db = client.db("spotifyAula");
 // const songCollection = await db.collection("songs").find({}).toArray();
